d81040645bbcdb4282bea9f40704e03f4f4b182d
Yohji <yohjiheonggg@gmail.com>
2025-04-22 23:08:24 +0800

    change meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "How to Flex vs Grid",
+  description: "Created with v0 huehue",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
